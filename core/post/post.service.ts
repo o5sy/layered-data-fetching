cd2f14875cf49c3f90643cf6fd5b776b9b0f58bb
o5sy <oosydev@gmail.com>
2026-03-12 18:23:20 +0900

    rename: 파일명 수정

--- core/post/post.service.ts
+++ core/post/post.service.ts
@@ -1,15 +1,15 @@
-import { httpClient, httpClientInstance } from './http-client';
+import { httpClient, httpClientInstance } from '../http-client';
 import {
   PostResponse,
   CreatePostRequest,
   UpdatePostRequest,
   PatchPostRequest,
-} from './models/api.model';
+} from '../models/api.model';
 
 // 역할: 리소스별 요청 서비스
 // 책임: 요청 메소드 관리
 // ! 테스트할 경우 목서비스를 생성하기 위해 타입 필요
-export const apiService = {
+export const postService = {
   // posts
   posts: async (): Promise<PostResponse[]> => {
     const res = await httpClientInstance.get<PostResponse[]>(`/posts`);
@@ -34,18 +34,4 @@ export const apiService = {
   deletePost: async (postId: number) => {
     await httpClientInstance.delete(`/posts/${postId}`);
   },
-
-  // comments
-  // comments: async (postId: number) => {
-  //     const res = await fetch(`${BASE_URL}/posts/${postId}/comments`);
-  //     if (!res.ok) {
-  //         throw new Error(`Failed to fetch comments for post with id ${postId}: ${res.statusText}`);
-  //     }
-  //     const data = await res.json();
-  //     return data;
-  // },
-  // comment: async (commentId: number) => {
-  //     const res = httpClient.get(`${BASE_URL}/comments/${commentId}`);
-  //     return res;
-  // }
 };
